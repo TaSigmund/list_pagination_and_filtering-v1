96a7df37f65e8501a5ecf24ab19ffb352999cd9e
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-05-18 23:06:00 +0200

    refactored show Page() and added appendPageLinks()
    The pagination is now working.

--- js/script.js
+++ js/script.js
@@ -5,56 +5,65 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 let listItems = document.querySelectorAll('.student-list')[0].children; //selects a collection of list elements
 let itemsPerPage = 10; // defines the number of list items to show per page
 
 
-
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   A showPage function that accepts the page number as its first parameter and the list to
+   be paginated as its second. A loop hides all the list items that shouldn't be displayed.
 ***/
 
-function showPage(page, itemsPerPage){
-for (let i = 0; i < (page * itemsPerPage)-itemsPerPage; i++) {
-   listItems[i].style.display = 'none';         // hides all previous pages
+function showPage(page, list){
+   let startIndex = (page * itemsPerPage)-itemsPerPage;
+   let endIndex = page * itemsPerPage
+for (let i = 0; i < listItems.length; i++) {
+   if(i < startIndex || i >= endIndex) {listItems[i].style.display = 'none'; }  
    }
-for (let i = page * itemsPerPage; i < listItems.length; i++) {
-listItems[i].style.display = 'none';            // hides all following pages
 }
-}
-showPage(1, itemsPerPage);
+showPage(1, listItems); //shows first page
 
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function appendPageLinks() {
 
+//Creates the container div inside the page div
+const container = document.createElement('div');
+container.className = 'pagination';
+const pageDiv = document.querySelector('.page');
+pageDiv.appendChild(container);
 
+//Creates the ul inside the container
+const ul = document.createElement('ul');
+container.appendChild(ul);
 
+//Creates the li elements inside the ul
+let numberOfPages = listItems.length / itemsPerPage;
+for (i = 0; i < numberOfPages; i++){
+   const li = document.createElement('li');
+   ul.appendChild(li)
+   const a = document.createElement('a');
+   a.href = '#';
+   a.textContent = i+1;
+   li.appendChild(a); 
+}
 
+//Adds the active class name to the first pagination link
+ul.firstElementChild.firstElementChild.className= 'active'
+
+//When clicked the handler reassigns the 'active' class and calls show page
+ul.addEventListener('click', (e)=>{if(e.target.tagName.toLowerCase() === 'a') {
+   let pageNumber = e.target.textContent;
+   let activeElements = document.getElementsByClassName('active');
+   for (let j = 0; j < activeElements.length; j++) {activeElements[j].className = '';} 
+   for (let i = 0; i < listItems.length; i++) {listItems[i].style.display = 'block';} //shows all listItems. Including the ones previously hidden.
+   showPage(pageNumber, listItems) //hides the listItems that shouldn't be displayed
+   e.target.className = 'active';
+}});
+}
+appendPageLinks();
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
