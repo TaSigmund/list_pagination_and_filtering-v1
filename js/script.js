c03993dd4cd2f9f619435dd4c3bce76b4e174507
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-05-21 19:40:22 +0200

    partially working search function
    Needs some refactoring. Doesn't show a message yet if there are no matches.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-let listItems = document.querySelectorAll('.student-list')[0].children; //selects a collection of list elements
-let itemsPerPage = 10; // defines the number of list items to show per page
-
+const listItems = document.querySelectorAll('.student-list')[0].children; //selects a collection of list elements
+const itemsPerPage = 10; // defines the number of list items to show per page
+let searchList = []; //listItems from a search
 
 /*** 
    A showPage function that accepts the page number as its first parameter and the list to
@@ -16,21 +14,23 @@ let itemsPerPage = 10; // defines the number of list items to show per page
 
 function showPage(page, list){
    let startIndex = (page * itemsPerPage)-itemsPerPage;
-   let endIndex = page * itemsPerPage
-for (let i = 0; i < listItems.length; i++) {
-   if(i < startIndex || i >= endIndex) {listItems[i].style.display = 'none'; }  
+   let endIndex = page * itemsPerPage;
+for (let i = 0; i < list.length; i++) {
+   list[i].style.display = 'block'; //resets the list
+   if(i < startIndex || i >= endIndex) {list[i].style.display = 'none'; }  
    }
 }
+
 showPage(1, listItems); //shows first page
 
-
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   A function to generate a pagination navigation. It creates a 
+   list of clickable page numbers and shows which page is currently active.
 ***/
-function appendPageLinks() {
 
-//Creates the container div inside the page div
+function appendPageLinks(list) {
+
+   //Creates the container div inside the page div
 const container = document.createElement('div');
 container.className = 'pagination';
 const pageDiv = document.querySelector('.page');
@@ -41,7 +41,7 @@ const ul = document.createElement('ul');
 container.appendChild(ul);
 
 //Creates the li elements inside the ul
-let numberOfPages = listItems.length / itemsPerPage;
+const numberOfPages = list.length / itemsPerPage;
 for (i = 0; i < numberOfPages; i++){
    const li = document.createElement('li');
    ul.appendChild(li)
@@ -64,6 +64,65 @@ ul.addEventListener('click', (e)=>{if(e.target.tagName.toLowerCase() === 'a') {
    e.target.className = 'active';
 }});
 }
-appendPageLinks();
+appendPageLinks(listItems);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/*** 
+   A search function to search for contacts in the list.
+***/
+
+//creates the searchDiv and adds 'student-search' class 
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search'
+
+//creates the input field, adds appropriate attributes and appends it to the searchDiv
+const inputField = document.createElement('input');
+inputField.type = 'text';
+inputField.id = 'search-input';
+inputField.placeholder = 'Search for students...';
+searchDiv.appendChild(inputField);
+
+//creates the button and appends it to the searchDiv
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+
+//selects the header and appends the div with the inputField
+const header = document.getElementsByClassName('page-header')[0];
+header.appendChild(searchDiv);
+searchDiv.appendChild(searchButton);
+
+function performSearch(searchInput, list) {
+   //let pageNumber = document.getElementsByClassName('active')[0].textContent; //determines the current page      
+   searchList = [];
+   for (let i = 0; i<list.length; i++){
+         if (searchInput.value.length === 0) {
+            showPage(1, list); break;}
+         else if(searchInput.value.length > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
+            searchList.push(list[i]);
+         }
+   } 
+   
+      removePaginationLinks()
+
+   if (searchInput.value.length === 0) {appendPageLinks(listItems)}
+   else if (searchList.length > 0) {
+      for (let i = 0; i<list.length; i++){list[i].style.display = 'none';} //resets the list
+      showPage(1, searchList);
+      appendPageLinks(searchList); //append appropriate menu
+   }
+}
+
+function removePaginationLinks() { 
+let navParent = document.getElementsByClassName('pagination')[0];
+const pageDiv = document.querySelector('.page');
+pageDiv.removeChild(navParent);} //removes pagination menu
+   
+
+searchButton.addEventListener('click', (e) => {
+   performSearch(inputField, listItems);
+   e.preventDefault();
+   console.log('button is functional!')});
+
+inputField.addEventListener('keyup', (e) => {
+   performSearch(inputField, listItems);
+   e.preventDefault()
+   console.log('input is functional!');})
