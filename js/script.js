2e8ff65099cd5c3b90f13f9cfbd5a4179e607510
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-05-22 15:48:12 +0200

    Improved formatting & comments

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ const itemsPerPage = 10; // defines the number of list items to show per page
 ***/
 
 function showPage(page, list){
-   let startIndex = (page * itemsPerPage)-itemsPerPage;
-   let endIndex = page * itemsPerPage;
+   const startIndex = (page * itemsPerPage)-itemsPerPage;
+   const endIndex = page * itemsPerPage;
    for (let i = 0; i < list.length; i++) {
       list[i].style.display = 'block'; //shows those list items that have been handed to the function
       if(i < startIndex || i >= endIndex) {list[i].style.display = 'none'; } //hides all items that are on pages that should not currently be displayed
@@ -30,39 +30,39 @@ showPage(1, listItems); //shows first page
 
 function appendPageLinks(list) {
 
-//Creates the container div inside the page div
-const container = document.createElement('div');
-container.className = 'pagination';
-const pageDiv = document.querySelector('.page');
-pageDiv.appendChild(container);
+   //Creates the container div inside the page div
+   const container = document.createElement('div');
+   container.className = 'pagination';
+   const pageDiv = document.querySelector('.page');
+   pageDiv.appendChild(container);
 
-//Creates the ul inside the container
-const ul = document.createElement('ul');
-container.appendChild(ul);
+   //Creates the ul inside the container
+   const ul = document.createElement('ul');
+   container.appendChild(ul);
 
-//Creates the li elements inside the ul
-const numberOfPages = list.length / itemsPerPage;
-for (i = 0; i < numberOfPages; i++){
-   const li = document.createElement('li');
-   ul.appendChild(li)
-   const a = document.createElement('a');
-   a.href = '#';
-   a.textContent = i+1;
-   li.appendChild(a); 
-}
+   //Creates the li elements inside the ul
+   const numberOfPages = list.length / itemsPerPage;
+   for (i = 0; i < numberOfPages; i++){
+      const li = document.createElement('li');
+      ul.appendChild(li)
+      const a = document.createElement('a');
+      a.href = '#';
+      a.textContent = i+1;
+      li.appendChild(a); 
+      }
 
-//Adds the active class name to the first pagination link
-ul.firstElementChild.firstElementChild.className= 'active'
+   //Adds the active class name to the first pagination link
+   ul.firstElementChild.firstElementChild.className= 'active'
 
-//When clicked the handler reassigns the 'active' class and calls show page
-ul.addEventListener('click', (e)=>{if(e.target.tagName.toLowerCase() === 'a') {
-   let pageNumber = e.target.textContent;
-   let activeElements = document.getElementsByClassName('active');
+   //When clicked the handler reassigns the 'active' class and calls show page
+   ul.addEventListener('click', (e)=>{if(e.target.tagName.toLowerCase() === 'a') {
+   const pageNumber = e.target.textContent;
+   const activeElements = document.getElementsByClassName('active');
    for (let j = 0; j < activeElements.length; j++) {activeElements[j].className = '';} 
    for (let i = 0; i < list.length; i++) {list[i].style.display = 'block';} //shows all listItems. Including the ones previously hidden.
    showPage(pageNumber, list) //hides the listItems that shouldn't be displayed
    e.target.className = 'active'; //reassigns the active class to the new page
-}});
+   }});
 }
 
 appendPageLinks(listItems); //shows pagination menu for the first page
@@ -99,9 +99,9 @@ function performSearch(searchInput, list) {
 
    //this part of the functions creates a list of search results
    let searchList = [];
-   let inputLength = searchInput.value.length;
-   for (let i = 0; i<list.length; i++){
-         if(inputLength > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
+   const inputLength = searchInput.value.length;
+   for (let i = 0; i < list.length; i++){
+         if(inputLength > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){ //checks whether the search string exists in the list
             searchList.push(list[i]); //creates the list
          }
          else if (inputLength > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()) === false) {
@@ -114,30 +114,33 @@ function performSearch(searchInput, list) {
    removePaginationLinks() //removes the pagination links that need to be refreshed
    if (inputLength === 0) {showPage(1, listItems); appendPageLinks(listItems)} //shows the start page if the input field is empty
    else if (searchList.length > 0) {
-      for (let i = 0; i<list.length; i++){list[i].style.display = 'none';} //resets the list
+      for (let i = 0; i < list.length; i++){list[i].style.display = 'none';} //resets the list
       showPage(1, searchList);
       appendPageLinks(searchList);     //append appropriate menu
    }
 }
+
+/*** 
+   A noResults function that prints a message to the screen if there are no search results.
+***/
 function noResults(searchInput, list) {
-let counter = 0 //keeps track of how many items match the search request
-const h2 = document.getElementsByTagName('h2')[0];
-for (let i = 0; i<list.length; i++){
-if (
-   (list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()))
-   )
-{counter += 1}}
-if (counter === 0) {h2.textContent = 'No results'}
-else {h2.textContent = 'Students'}
+   let counter = 0 //keeps track of how many items match the search request
+   const h2 = document.getElementsByTagName('h2')[0];
+   for (let i = 0; i < list.length; i++){
+      if (list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()))
+         {counter += 1} //increases the counter by one if there are search results
+   }
+   if (counter === 0) {h2.textContent = 'No results found'} //changes the h2 if there are no search results
+   else {h2.textContent = 'Students'} //resets the h2 if there are search results
 }
 
 
 //removes pagination menu
 function removePaginationLinks() { 
-let navParent = document.getElementsByClassName('pagination')[0];
-const pageDiv = document.querySelector('.page');
-if (navParent){      //checks if there is an item that can be remove
-pageDiv.removeChild(navParent);}} 
+   const navParent = document.getElementsByClassName('pagination')[0];
+   const pageDiv = document.querySelector('.page');
+   if (navParent){pageDiv.removeChild(navParent);} //checks if there is an item that can be remove before it removes it
+} 
    
 //fires when the search button gets clicked
 searchButton.addEventListener('click', (e) => {
