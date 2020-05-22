1eef0b5df46c84995bfc5fa4c069eef8c65421f5
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-05-22 20:00:21 +0200

    refactoring
    incorporated noResults function into performSearch function
    
    reordered code for better readability
    
    code now only uses one method for selecting elements by className

--- js/script.js
+++ js/script.js
@@ -3,9 +3,43 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-const listItems = document.querySelectorAll('.student-list')[0].children; //selects a collection of list elements
+/*** 
+   Global variables to hold the list of students & how many items should be displayed per page.
+***/
+
+const listItems = document.getElementsByClassName('student-list')[0].children; //selects a collection of list elements
 const itemsPerPage = 10; // defines the number of list items to show per page
 
+/*** 
+   The following lines of code create a search field and a search button.
+***/
+
+//creates the searchDiv and adds 'student-search' class 
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search'
+
+//creates the input field, adds appropriate attributes and appends it to the searchDiv
+const inputField = document.createElement('input');
+inputField.type = 'text';
+inputField.id = 'search-input';
+inputField.placeholder = 'Search for students...';
+searchDiv.appendChild(inputField);
+
+//creates the button and appends it to the searchDiv
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+
+//selects the header and appends the div with the inputField
+const header = document.getElementsByClassName('page-header')[0];
+header.appendChild(searchDiv);
+searchDiv.appendChild(searchButton);
+
+/*** 
+   Functions that get called on page load
+***/
+
+showPage(1, listItems); //shows first page
+appendPageLinks(listItems); //shows pagination menu for the first page
 
 /*** 
    A showPage function that accepts the page number as its first parameter and the list to
@@ -21,8 +55,6 @@ function showPage(page, list){
    }
 }
 
-showPage(1, listItems); //shows first page
-
 /*** 
    A function to generate a pagination navigation. It creates a 
    list of clickable page numbers and shows which page is currently active.
@@ -33,7 +65,7 @@ function appendPageLinks(list) {
    //Creates the container div inside the page div
    const container = document.createElement('div');
    container.className = 'pagination';
-   const pageDiv = document.querySelector('.page');
+   const pageDiv = document.getElementsByClassName('page')[0];
    pageDiv.appendChild(container);
 
    //Creates the ul inside the container
@@ -65,50 +97,32 @@ function appendPageLinks(list) {
    }});
 }
 
-appendPageLinks(listItems); //shows pagination menu for the first page
 
 /*** 
-   The following lines of code create a search field and a search button.
-***/
-
-//creates the searchDiv and adds 'student-search' class 
-const searchDiv = document.createElement('div');
-searchDiv.className = 'student-search'
-
-//creates the input field, adds appropriate attributes and appends it to the searchDiv
-const inputField = document.createElement('input');
-inputField.type = 'text';
-inputField.id = 'search-input';
-inputField.placeholder = 'Search for students...';
-searchDiv.appendChild(inputField);
-
-//creates the button and appends it to the searchDiv
-const searchButton = document.createElement('button');
-searchButton.textContent = 'Search';
-
-//selects the header and appends the div with the inputField
-const header = document.getElementsByClassName('page-header')[0];
-header.appendChild(searchDiv);
-searchDiv.appendChild(searchButton);
-
-/*** 
-   A search function to search for contacts in the list.
+   A search function to search for contacts in the list. If no results are found a message is printed to the page.
 ***/
 
 function performSearch(searchInput, list) {  
 
    //this part of the functions creates a list of search results
    let searchList = [];
+   let counter = 0 //keeps track of how many items match the search request
+   const h2 = document.getElementsByTagName('h2')[0];
    const inputLength = searchInput.value.length;
    for (let i = 0; i < list.length; i++){
          if(inputLength > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){ //checks whether the search string exists in the list
             searchList.push(list[i]); //creates the list
+            counter += 1;
          }
          else if (inputLength > 0 && list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()) === false) {
             for (let i = 0; i < list.length; i++) {
                list[i].style.display = 'none';} //hides the list if there are no matches
          }
    } 
+
+   //this part of the function prints a message to the screen if no list item matches the search request.
+   if (counter === 0 && inputLength > 0) {h2.textContent = 'No results found'} //changes the h2 if there are no search results
+   else {h2.textContent = 'Students'} //resets the h2 if there are search results
    
    //this part of the function displays the search results
    removePaginationLinks() //removes the pagination links that need to be refreshed
@@ -120,25 +134,10 @@ function performSearch(searchInput, list) {
    }
 }
 
-/*** 
-   A noResults function that prints a message to the screen if there are no search results.
-***/
-function noResults(searchInput, list) {
-   let counter = 0 //keeps track of how many items match the search request
-   const h2 = document.getElementsByTagName('h2')[0];
-   for (let i = 0; i < list.length; i++){
-      if (list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()))
-         {counter += 1} //increases the counter by one if there are search results
-   }
-   if (counter === 0) {h2.textContent = 'No results found'} //changes the h2 if there are no search results
-   else {h2.textContent = 'Students'} //resets the h2 if there are search results
-}
-
-
 //removes pagination menu
 function removePaginationLinks() { 
    const navParent = document.getElementsByClassName('pagination')[0];
-   const pageDiv = document.querySelector('.page');
+   const pageDiv = document.getElementsByClassName('page')[0];
    if (navParent){pageDiv.removeChild(navParent);} //checks if there is an item that can be removed before it removes it
 } 
    
